ab2f3ded8894ff4bc8eb3ff132f3bffaf3e5e7b0
Huy nq <huy.nguyenquang@evotek.vn>
2023-06-04 22:48:11 +0700

    init

---
+++ src/test/java/anhtester/com/data/reqres/AuthenticationRequest.java
@@ -0,0 +1,13 @@
+package anhtester.com.data.reqres;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class AuthenticationRequest implements Serializable {
+    private String email;
+    private String password;
+}
